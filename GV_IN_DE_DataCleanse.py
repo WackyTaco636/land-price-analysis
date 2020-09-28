4cbc69515e82455f84fb331cf1842db750b591ec
Chris <55195100+WackyTaco636@users.noreply.github.com>
2020-09-28 12:56:46 +0200

    Update GV_IN_DE_DataCleanse.py
    Modified dfc (dataframe clean) to include column 'fed'.
    Added the function fed_summary.

--- GV_IN_DE_DataCleanse.py
+++ GV_IN_DE_DataCleanse.py
@@ -10,7 +10,7 @@ cols = [column[0] for column in tbl.description]
 df = pd.DataFrame(tbl,columns=cols).round()
 dfk = df[df['marketingtype'] == 'Kauf']
 # df = df[df['sqmprice'] > 10]
-dfc = dfk.loc[:,['offer','zip','area','price']].dropna()
+dfc = dfk.loc[:,['offer','fed','zip','area','price']].dropna()
 dfc = dfc[(dfc['area'] > 100) | (dfc['price'] > 1000)].round()
 # dfc.to_csv('/home/chris/Documents/01_Projects/01_Grundstückvaluierung/04_Analysis/01_Data/dfc')
 ### Create a list of available zip-codes based on the scraped data where there are at least two samples for linear regression
@@ -21,5 +21,12 @@ vlucnts = dfc.zip.value_counts()
 # 'sample_size' is defined in GV_streamlit.py as the slider bar.
 def zip_samples(sample_size) :
     zips = list(vlucnts[vlucnts >= sample_size].index.sort_values())
-
     return zips
+
+# 'select_fed' is determined via dropdown in GV_streamlit.py.
+def fed_summary(select_fed) :
+    dfcf = dfc[(dfc['fed'] == select_fed)]
+    dfcf = dfcf.loc[:,['area','price']]
+    dfcf['sqmprice'] = dfcf['price'] / dfcf['area']
+    desc = dfcf.describe().round().astype(int)
+    return print(desc)
